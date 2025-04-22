e219403a2933547813df83d2bc5861afdbedca8b
vr359490 <vr359490@gmail.com>
2025-04-22 10:11:43 -0500

    Add files via upload

--- mathDash_s3.py
+++ mathDash_s3.py
@@ -1,22 +1,10 @@
 import pandas as pd
-from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import TimeoutException
 import dash
 from dash import dash_table, dcc, html, Input, Output, callback
-import pprint
-import threading
 import os
-from dotenv import load_dotenv
-import webbrowser
 import plotly.graph_objects as go
-from io import StringIO, BytesIO
+from io import BytesIO
 
-import time
 import calendar
 import datetime
 
@@ -31,94 +19,18 @@ s3 = boto3.client("s3",
                       aws_secret_access_key=AWS_SECRET_KEY,
                       region_name="us-east-2")
 
-def get_browser(url):
-    options = Options()
-    #options.add_argument("--headless")
-    options.add_argument('--disable-dev-shm-usage')
-    browser = webdriver.Chrome(options=options)
-    browser.get(url)
-    return browser
-
-def login(browser, wait):
-
-    # Load credentials securely
-    # load_dotenv()
-    USERNAME = os.getenv("MATHNASIUM_USERNAME")
-    PASSWORD = os.getenv("MATHNASIUM_PASSWORD")
-
-    # Login
-    username_field = browser.find_element(By.ID, "UserName")
-    username_field.send_keys(USERNAME)
-
-    password_field = browser.find_element(By.ID, "Password")
-    password_field.send_keys(PASSWORD)
-
-    login_field = wait.until(EC.presence_of_all_elements_located((By.ID, "login")))
-    browser.execute_script("arguments[0].click();", login_field[0])
-
-def learn_plan_scrape(browser, wait, student_roster):
+def learn_plan_scrape():
 
     inactive_students = []
 
     all_student_LP = {}
 
-    # url = 'https://radius.mathnasium.com/LearningPlan'
-    # browser.get(url)
-
-    # studentDropdown = wait.until(EC.presence_of_all_elements_located((By.ID, "studentDropDownList")))
-
-    # showInactiveCheckBox = wait.until(EC.presence_of_all_elements_located((By.ID, "showinactive")))
-    # browser.execute_script("arguments[0].click();", showInactiveCheckBox[0])
-
-    # for i in range(len(student_roster)):
-        
-    #     full_name = student_roster["Full Name"][i]
-
-    #     browser.execute_script("arguments[0].click();", studentDropdown[0])
-
-    #     string_match = "//*[contains(text(), " + "'" + full_name + "'" + ")]"
-
-    #     try:
-    #         # wait up to 10 seconds for the element to be clickable
-    #         student_dropdown_item = wait.until(EC.element_to_be_clickable((By.XPATH, string_match)))
-    #         browser.execute_script("arguments[0].click();", student_dropdown_item)
-
-    #     except TimeoutException:
-    #         print("Element not clickable—doing something else instead.")
-    #         inactive_students.append(full_name)
-    #         continue
-        
-    #     lp = export_excel(wait)
-    #-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0
-    # def round2(s3):
-
-    #     ulti = s3.list_objects_v2(Bucket=bucket, Prefix="learning_plans/")
-    #     pprint.pprint(ulti)
-    #     xlsx_keys = [
-    #             obj["Key"] for obj in ulti.get("Contents",[])
-    #             if obj["Key"].endswith(".xlsx")
-    #         ]
-    #     pprint.pprint(xlsx_keys)
-    #     for obj in xlsx_keys:
-
-    #         response = s3.get_object(Bucket=bucket, Key=obj)
-            
-    #         obj_content = response["Body"].read()
-    #         obj_content = BytesIO(obj_content)
-
-    #         lp = pd.read_excel(obj_content)
-    #     return lp
-
     ulti = s3.list_objects_v2(Bucket=bucket, Prefix="learning_plans/")
-    pprint.pprint(ulti)
 
     xlsx_keys = [
             obj["Key"] for obj in ulti.get("Contents",[])
             if obj["Key"].endswith(".xlsx")
         ]
-
-    #for i in range(len(student_roster)):
-
 
     for obj in xlsx_keys:
 
@@ -129,7 +41,6 @@ def learn_plan_scrape(browser, wait, student_roster):
 
         lp = pd.read_excel(obj_content)
 
-        print(lp)
         full_name=lp['Student'][0].strip()
 
         # Separate ID and title/name of each PK. This helps de-clutter the LP chart display
@@ -156,71 +67,14 @@ def learn_plan_scrape(browser, wait, student_roster):
 
     return all_student_LP, inactive_students
 
-def sessions_scrape(wait):
-
-    # centerDropdown = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='theGCSMulti']//input[@class='k-input k-readonly']")))
-
-    # time.sleep(2)
-
-    # centerDropdown.send_keys('v') # 'v' for Verona
-
-    # time.sleep(5)
-    # centerDropdown.send_keys(Keys.ENTER)
-
-    # time.sleep(3)
-    # table = wait.until(EC.presence_of_all_elements_located((By.ID, "gridRoster")))
-    # roster = table[0].get_attribute('outerHTML')
-
-    # roster_df = pd.read_html(StringIO(roster))[0]
-
-    # sessions_left = roster_df[['First Name', 'Last Name','Membership Type', 'Remaining']]
-
-    # sessions_left.to_csv('sessions_left.csv', index=False)
+def sessions_scrape():
 
     sessions_left = down(s3, 'sessions_left.csv')
-    print(sessions_left)
-    print(sessions_left.columns)
 
     return sessions_left
 
-def dwp_scrape(browser, wait):
-
-    # Should we have different functions for scraping ALL data vs. scraping the
-    # most recent? Or can we get away with scraping all data manually one time?
-
-    url = "https://radius.mathnasium.com/DigitalWorkoutPlan/Report"
-    browser.get(url)
-
-    quickDateList = wait.until(EC.element_to_be_clickable((By.XPATH, "//span[@class='k-widget k-dropdown form-control searchGridDropDown qdFilter']")))
-    quickDateList.click()
-
-    time.sleep(2)
-
-    # The following is pulled from the UI element. It tells what number corresponds to what time range
-    # 3 is "Last 7 Days" for example
-    # {"dataSource":[{"Text":"Last 90 days","Value":"-1"},{"Text":"Last 60 days","Value":"1"},{"Text":"Last 30 days","Value":"2"},{"Text":"Last 7 days","Value":"3"},{"Text":"This month","Value":"0"},{"Text":"Today","Value":"7"}],"dataTextField":"Text","dataValueField":"Value","optionLabel":"Select"});});
-
-    # Can we figure out how to do longer date ranges? We would need to interact with the Kendo calender element
-
-    browser.execute_script("$('#dwpQuickDate').data('kendoDropDownList').value('-1');")  # 3 corresponds with "Last 7 Days"
-    browser.execute_script("$('#dwpQuickDate').data('kendoDropDownList').trigger('change');")  # Trigger change event
-
-    searchButton = browser.find_element(By.ID, "btnsearch")
-
-    time.sleep(3)
-    searchButton.click()
-
-    time.sleep(5)
-
-    data = export_excel(wait)
-
-    pk_completion = str_process(data)
-
-    return pk_completion
-
+# Import files from AWS S3
 def down(s3, filename):
-
-    #filename = None
 
     ulti = s3.list_objects_v2(Bucket=bucket)
     
@@ -236,30 +90,27 @@ def down(s3, filename):
         response = s3.get_object(Bucket=bucket, Key=obj)
         
         obj_content = response["Body"].read()
-        obj_content = BytesIO(obj_content)
-        if ".csv" in filename:
-            zaza = pd.read_csv(obj_content)
-        else:
-            zaza = pd.read_excel(obj_content)
-        
 
-    return zaza
+        obj_content = BytesIO(obj_content)
+
+        if ".csv" in filename:
+            df = pd.read_csv(obj_content)
+        else:
+            df = pd.read_excel(obj_content)
+
+    return df
 
 def dwp_scrape_ALL():
-
-    # path = "/Users/victorruan/Desktop/mathnasium_dash_files/DWP_Report_(All).xlsx"
-
-    # data = pd.read_excel(path)
 
     filename = "DWP_Report_(All).xlsx"
 
     data = down(s3, filename)
 
-    pk_completion = str_process(data)
+    pk_completion = dwp_process(data)
 
     return pk_completion
 
-def str_process(data):
+def dwp_process(data):
     
     pk_completion = {}
 
@@ -359,37 +210,6 @@ def str_process(data):
         pk_completion[student]["Average Sessions to Master PK"] = pk_completion[student]["Total Sessions Worked On"]/pk_completion[student]["Number of PKs"] 
     
     return pk_completion
-
-def export_excel(wait):
-
-    download_path = "/Users/victorruan/Downloads"
-
-    # List current files in Downloads
-    pre = os.listdir(download_path)
-
-    export = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Excel')]")))
-    export.click()
-
-    post = os.listdir(download_path)
-
-    # Check download files before and after hitting export to check completed downloaded
-    while len(pre)==len(post):
-        time.sleep(1)
-        post = os.listdir(download_path)
-
-    new_files = set(post) - set(pre)
-
-    # If there's a new file, pop it from the set
-    filename = new_files.pop() if new_files else None
-
-    if filename is None:
-        raise FileNotFoundError("No new file was downloaded.")
-
-    path = download_path + "/" + filename
-
-    data = pd.read_excel(path)
-
-    return data
 
 def app_layout(app, process_df, student_roster, attendance_df, all_attendance_df, low_attend_report):
 
@@ -605,7 +425,8 @@ def app_layout(app, process_df, student_roster, attendance_df, all_attendance_df
 
     @callback(
         Output('pandas-dropdown-3', 'value'),
-        Input('pandas-dropdown-1', 'value'))
+        Input('pandas-dropdown-1', 'value')
+        )
     def update_dropdown3_val(student):
         assigned = pd.to_datetime(process_df[student]['Assigned'])
     
@@ -622,7 +443,8 @@ def app_layout(app, process_df, student_roster, attendance_df, all_attendance_df
     @callback(
         Output('lp-table', 'data'),
         Input('pandas-dropdown-1', 'value'),
-        Input('pandas-dropdown-3', 'value'))
+        Input('pandas-dropdown-3', 'value')
+        )
     def update_LP(student, learning_plan_name):
         chosen_LP = process_df[student]
         chosen_LP = chosen_LP[chosen_LP['Learning Plan Name']==learning_plan_name].to_dict('records')
@@ -685,11 +507,6 @@ def app_layout(app, process_df, student_roster, attendance_df, all_attendance_df
             id='pandas-dropdown-2')   
     ],style={'display':'inline-block','width':'17%', 'verticalAlign':'top'}),
     dash.page_container])
-# --------------------------------------------------------------------------------------------------------------
-# def open_browser():
-#     time.sleep(2)  # Allow a brief moment for the server to start
-#     webbrowser.open("http://127.0.0.1:8050/")
-#     quit()
 
 def pk_process_fig(student, pk_completion):
 
@@ -730,9 +547,6 @@ def pk_process_fig(student, pk_completion):
     return fig
 
 def attend_scrape(sessions_left):
-
-    # path = "/Users/victorruan/Desktop/mathnasium_dash_files/Attendance_(All).xlsx"
-    # attendance = pd.read_excel(path)
 
     attendance = down(s3, "Attendance_(All).xlsx")
 
@@ -790,7 +604,6 @@ def attend_process(attendance, sessions_left):
     avg_attendance_col = [round(average, 2) for average in avg_attendance_col]
     sessions_left['Avg Attend/m'] = avg_attendance_col
 
-    # We should probably send sessions_left in full then separate in to attendance_df and low_attend_report later
     low_attend_report = sessions_left.copy(deep = True)
 
     attendance_df = sessions_left[['Full Name','Membership Type','Remaining','Avg Attend/m','This Month Attendance']]
@@ -809,44 +622,14 @@ def string_check(string):
 app = dash.Dash(__name__, use_pages=True, pages_folder="")
 server = app.server
 
-#def main():
-print("run")
-
-# Initialize browser at given URL
-# Scraping Roster first allows us to gather list of student names
-# url = 'https://radius.mathnasium.com/Attendance/Roster'
-# browser = get_browser(url)
-
-browser = ""
-wait = ""
-
-# wait = WebDriverWait(browser, 10)
-
-# Grab credentials and login
-#login(browser, wait)
-
-# Web scrape stage
-sessions_left = sessions_scrape(wait)
+sessions_left = sessions_scrape()
 sessions_left['Full Name'] = sessions_left['First Name'].astype(str) + " " + sessions_left['Last Name'].astype(str)
-#sessions_left = sessions_left.head(2)
 
-# Do we care about attendance of inactive students?
 attendance_df, all_attendance_df, low_attend_report = attend_scrape(sessions_left)
-learn_plan_df, inactive_students = learn_plan_scrape(browser, wait, sessions_left)
+learn_plan_df, inactive_students = learn_plan_scrape()
 print("Unable to find data for the following students: ", inactive_students)
 
-# Done scraping
-#browser.quit()
-
-# Initialize CSS styling and dash app
-# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-# CSS is placed in styles.css
-
-# App settings and layout
 app_layout(app, learn_plan_df, sessions_left['Full Name'], attendance_df, all_attendance_df, low_attend_report)
 
-# Run dash app
 if __name__ == '__main__':
-    #threading.Thread(target=open_browser, daemon=True).start()  # Start browser in background
     app.run() 
-#main()
